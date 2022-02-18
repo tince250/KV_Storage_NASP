64485321e1aa53cf27e81801e2935dff6eb2952e
Nebojša Vuga <vuga.sv53.2020@uns.ac.rs>
2022-02-18 19:50:51 +0100

    Apdejtovan main fajl
    Dodat token bucket

--- Engine/main/mainGo.go
+++ Engine/main/mainGo.go
@@ -1,9 +1,13 @@
 package main
 
+import (
+	"fmt"
+)
+
 func menu(){
-	//PUT 	 GOTOVO
-	//GET	 GOTOVO
-	//DELETE GOTOVO
+	//PUT X
+	//GET
+	//DELETE X
 	//START COMPACTION
 	defVals := defValues{}
 	defVals.getDefaultValues("config/config.yml")
@@ -11,7 +15,10 @@ func menu(){
 	memtable.initMemtableWithPassedValues(defVals.MemtableSize, defVals.Threshold, defVals.WalThreshold, defVals.MaxHeight)
 	lru := &LruCache{}
 	lru.initializeLRU(defVals.CacheSize) //TODO: DEFAULT VREDNOSTI ZA CACHE
+	tb := &TokenBucket{}
+	tb.initTokenBucket(5,5)
 	for{
+
 		fmt.Println("1. PUT")
 		fmt.Println("2. GET")
 		fmt.Println("3. DELETE")
@@ -31,20 +38,36 @@ func menu(){
 			var value string
 			fmt.Scanln(&value)
 			memtable.insertToMemtable(key, []byte(value), 0)
+			if tb.addToken() {
+				memtable.insertToMemtable(key, []byte(value), 0)
+			}else{
+				fmt.Println("Token bucket full.")
+			}
 		}else if decision == "2"{
 			fmt.Println("GET")
 			fmt.Println("Enter Key:\n>> ")
 			var key string
 			fmt.Scanln(&key)
-			get(memtable, lru, key)
-
+			if tb.addToken() {
+				node := get(memtable, lru, key)
+				if node != nil {
+					fmt.Println(node.key)
+					fmt.Println(node.value)
+				}
+			}else{
+				fmt.Println("Token bucket full.")
+			}
 		}else if decision == "3"{
 			fmt.Println("DELETE")
 			fmt.Println("Enter Key:\n>> ")
 			var key string
 			fmt.Scanln(&key)
-			memtable.insertToMemtable(key, []byte(""), 2)
-			lru.deleteFromCache(key)
+			if tb.addToken() {
+				memtable.insertToMemtable(key, []byte(""), 2)
+				lru.deleteFromCache(key)
+			}else{
+				fmt.Println("Token bucket full.")
+			}
 		}else if decision == "4"{
 			fmt.Println("COMPACT FILES")
 		}else if decision == "5"{
@@ -56,7 +79,6 @@ func menu(){
 
 	}
 }
-
 
 func main(){
 	menu()
