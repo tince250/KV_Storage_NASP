7fee013c1bec8ce3dc2784e7859b40330071134f
Nebojša Vuga <vuga.sv53.2020@uns.ac.rs>
2022-02-18 12:54:24 +0100

    Apdejtovan main fajl
    Dodat meni sa Put/Get/Delete operacijama

--- Engine/main/mainGo.go
+++ Engine/main/mainGo.go
@@ -1,37 +1,63 @@
 package main
 
-func main(){
-	/*
-		1. Ucitavamo default vrednosti za memtable i wal 					GOTOVO
-		2. Kreiramo wal segment, ukoliko ne postoji wal pravimo ga			!!!!!!
-		3. Kreiramo skip listu, ucitamo je u memoriju 						GOTOVO
-		4. Postavljamo default vrednosti za velicinu i segment				GOTOVO
-		5. Upisivanje u memtable moze poceti!								GOTOVO
-		6. Kad dodje do tresholda, ide flush								!!!!!!
-		7. Rekreacija wal-a - ucitavamo redom podatke iz walova 			GOTOVO
-			(od prvog do poslednjeg), i redom
-			primenjujemo izmene na memtable
-	*/
-	defVals := &defValues{}
+func menu(){
+	//PUT 	 GOTOVO
+	//GET	 GOTOVO
+	//DELETE GOTOVO
+	//START COMPACTION
+	defVals := defValues{}
 	defVals.getDefaultValues("config/config.yml")
-	sl := &SkipList{}
-	sl.createSkipList(defVals.MaxHeight, defVals.Threshold)
-	a := readFullData("wal/wal_0001.log")
-	sl.inserFromWal(a)
+	memtable := &Memtable{}
+	memtable.initMemtableWithPassedValues(defVals.MemtableSize, defVals.Threshold, defVals.WalThreshold, defVals.MaxHeight)
+	lru := &LruCache{}
+	lru.initializeLRU(defVals.CacheSize) //TODO: DEFAULT VREDNOSTI ZA CACHE
+	for{
+		fmt.Println("1. PUT")
+		fmt.Println("2. GET")
+		fmt.Println("3. DELETE")
+		fmt.Println("4. COMPACT")
+		fmt.Println("5. EXIT")
+
+		var decision string
+		fmt.Print("\nChoose option:\n>> ")
+		// Taking input from user
+		fmt.Scanln(&decision)
+		if decision == "1"{
+			fmt.Println("PUT")
+			fmt.Println("Enter Key:\n>> ")
+			var key string
+			fmt.Scanln(&key)
+			fmt.Println("Enter Value:\n>> ")
+			var value string
+			fmt.Scanln(&value)
+			memtable.insertToMemtable(key, []byte(value), 0)
+		}else if decision == "2"{
+			fmt.Println("GET")
+			fmt.Println("Enter Key:\n>> ")
+			var key string
+			fmt.Scanln(&key)
+			get(memtable, lru, key)
+
+		}else if decision == "3"{
+			fmt.Println("DELETE")
+			fmt.Println("Enter Key:\n>> ")
+			var key string
+			fmt.Scanln(&key)
+			memtable.insertToMemtable(key, []byte(""), 2)
+			lru.deleteFromCache(key)
+		}else if decision == "4"{
+			fmt.Println("COMPACT FILES")
+		}else if decision == "5"{
+			fmt.Println("Exiting app...")
+			break
+		}else{
+			fmt.Println("Invalid input. Try Again.\n")
+		}
+
+	}
+}
 
 
-	sl.addNode("marko", []byte("markovic"))
-	//sl.addNode("mirko", []byte("cepes"))
-	//sl.addNode("Nenad", []byte("Rozic"))
-	//sl.addNode("Aleksandar", []byte("Miric"))
-	//sl.addNode("Milan", []byte("Miric"))
-	//sl.addNode("Dejan", []byte("Miric"))
-	sl.addNode("Borko", []byte("Miric"))
-	//sl.addNode("Borjan", []byte("Mirkovic"))
-	sl.logicalDelete("Borko")
-
-	sl.printList()
-
-
-
+func main(){
+	menu()
 }
